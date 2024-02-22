50739c8335a30b55769d3a74dcaa2990be93f38e
JulienKhlt <julien.khlaut@gmail.com>
2024-02-22 16:01:03 +0000

    leap

--- nvim-custom/plugins.lua
+++ nvim-custom/plugins.lua
@@ -75,9 +75,32 @@ local plugins = {
     lazy = false,
   },
   {
-    'ggandor/lightspeed.nvim',
-    event = "VeryLazy"
+    'ggandor/leap.nvim',
+     enabled = true,
+     keys = {
+       { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+       { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+       { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+     },
+     config = function(_, opts)
+       local leap = require("leap")
+       for k, v in pairs(opts) do
+         leap.opts[k] = v
+       end
+       leap.add_default_mappings(true)
+       vim.keymap.del({ "x", "o" }, "x")
+       vim.keymap.del({ "x", "o" }, "X")
+      leap.user.set_repeat_keys('<cr>', '<bs>', {
+        -- False by default. If set to true, the keys will work like the
+        -- native semicolon/comma, i.e., forward/backward is understood in
+        -- relation to the last motion.
+        relative_directions = true,
+        -- By default, all modes are included.
+        modes = {'n', 'x', 'o'},
+      })
+     end,
   },
+  { "tpope/vim-repeat", event = "VeryLazy" },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     lazy = false,
