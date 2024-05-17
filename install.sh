7634e251064ffb4bffa1811775b7003dca23aa3c
JulienKhlt <julien.khlaut@gmail.com>
2024-05-17 10:00:58 +0000

    add script for installation

---
+++ install.sh
@@ -0,0 +1,22 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+rm .zshrc
+ln -s dotfiles/.zshrc .zshrc
+ln -s dotfiles/.p10k.zsh .p10k.zsh
+ln -s dotfiles/.gitconfig .gitconfig
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+
+
+git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
+ln -s $(pwd)/dotfiles/nvim-custom/ .config/nvim/lua/custom
+
+pipx install poetry
