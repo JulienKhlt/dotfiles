4aeff54856b141fc17479f2f4905f27be46b05ce
JulienKhlt <julien.khlaut@gmail.com>
2024-05-17 10:12:49 +0000

    update install

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $(pwd)/dotfiles/tmux .config/tmux
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
