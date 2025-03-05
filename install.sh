fe15d2b245de37e91850a0adc1a2338be31e48a0
JulienKhlt <julien.khlaut@gmail.com>
2025-03-05 13:22:11 +0000

    new install

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s $(pwd)/dotfiles/tmux .config/tmux
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-rm .zshrc
-ln -s dotfiles/.zshrc .zshrc
-ln -s dotfiles/.p10k.zsh .p10k.zsh
-ln -s dotfiles/.gitconfig .gitconfig
-
+curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 
+# curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+#
+rm .zshrc
+ln -s dotfiles/.zshrc .zshrc
+ln -s dotfiles/.gitconfig .gitconfig
+ln -s dotfiles/starship.toml .config/starship.toml
+ln -s dotfiles/nvim .config/nvim
 
-git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
-ln -s $(pwd)/dotfiles/nvim-custom/ .config/nvim/lua/custom
+# install brew
+curl -sS https://webi.sh/brew | sh
+source ~/.config/envman/PATH.env
+
+brew install lazygit
+brew install nushell
 
 pipx install poetry
--- nvim-custom/configs/treesitter.lua
+++
@@ -1,4 +0,0 @@
-local options = {
-  ensure_installed = { "lua", "javascript", "typescript", "tsx", "python", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "json", "yaml" },
-}
-return options
